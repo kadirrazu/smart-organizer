d48e9f6f7935a8a5844b06939c7de955925e2e64
razu121@gmail.com <razu121@gmail.com>
2018-10-28 15:02:16 +0600

    Commit from CC PC - 28.10.2018 @ 03:00PM

--- resources/views/expense-tracker/partials/navbar.blade.php
+++ resources/views/expense-tracker/partials/navbar.blade.php
@@ -1,10 +1,9 @@
 <!-- partial:partials/_navbar.html -->
 <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
   <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-    <a class="navbar-brand brand-logo" href="index.html">
-	<!--<img src="images/logo.svg" alt="logo"/>-->
-	ExpenseTracker 
-</a>
+    <a class="navbar-brand brand-logo" href="{!! url('et/dashboard') !!}">
+    	ExpenseTracker 
+    </a>
     <a class="navbar-brand brand-logo-mini" href="index.html">
 	<img src="images/logo-mini.svg" alt="logo"/>
 </a>
@@ -22,24 +21,43 @@
   <li class="nav-item nav-profile dropdown">
         <a class="nav-link dropdown-toggle" id="profileDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
           <div class="nav-profile-text">
-            <p class="mb-1 text-black">David Greymaax</p>
+            <p class="mb-1 text-black">Subsystems</p>
           </div>
         </a>
         <div class="dropdown-menu navbar-dropdown" aria-labelledby="profileDropdown">
-          <a class="dropdown-item" href="#">
-            <i class="mdi mdi-cached mr-2 text-success"></i>
-            Activity Log
+          <a class="dropdown-item" href="{!! url('subsystem-selector') !!}">
+            <i class="mdi mdi-collage mr-2 text-success"></i>
+            All Subsystems
+          </a>
+          <a class="dropdown-item" href="{!! url('hl/dashboard') !!}">
+            <i class=" mdi mdi-bookmark-check mr-2 text-success"></i>
+            Health Log
+          </a>
+          <a class="dropdown-item" href="{!! url('pb/dashboard') !!}">
+            <i class=" mdi mdi-bookmark-check mr-2 text-success"></i>
+            Phone Book
+          </a>
+          <a class="dropdown-item" href="{!! url('lm/dashboard') !!}">
+            <i class=" mdi mdi-bookmark-check mr-2 text-success"></i>
+            Library Manager
           </a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">
+          <a class="dropdown-item" href="{!! url('logout') !!}">
             <i class="mdi mdi-logout mr-2 text-primary"></i>
             Signout
           </a>
         </div>
+      </li>
+
+      <li class="nav-item nav-logout d-none d-lg-block">
+        <a class="nav-link" href="{!! url('/et/activity-log') !!}">
+          <i class="mdi mdi-cached mr-2 text-success"></i>
+            Activity Log
+        </a>
       </li> 
 
       <li class="nav-item nav-logout d-none d-lg-block">
-        <a class="nav-link" href="#">
+        <a class="nav-link" href="{!! url('logout') !!}">
           <i class="mdi mdi-power"></i>
         </a>
       </li>
--- resources/views/expense-tracker/partials/sidebar.blade.php
+++ resources/views/expense-tracker/partials/sidebar.blade.php
@@ -3,21 +3,16 @@
   <ul class="nav">
 
     <li class="nav-item nav-profile">
-      <a href="#" class="nav-link">
-        <div class="nav-profile-image">
-          <img src="{!! asset('theme/images/faces/face1.jpg') !!}" alt="profile">
-          <span class="login-status online"></span> <!--change to offline or busy as needed-->              
-        </div>
+      <a href="{!! url('et/user/show/'.Auth::user()->id) !!}" class="nav-link">
         <div class="nav-profile-text d-flex flex-column">
-          <span class="font-weight-bold mb-2">David Grey. H</span>
-          <span class="text-secondary text-small">Project Manager</span>
+          <span class="font-weight-bold">{!! Auth::user()->name !!}</span>
         </div>
         <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
       </a>
     </li>
 
     <li class="nav-item">
-      <a class="nav-link" href="index.html">
+      <a class="nav-link" href="{!! url('et/dashboard') !!}">
         <span class="menu-title">Dashboard</span>
         <i class="mdi mdi-home menu-icon"></i>
       </a>
@@ -33,15 +28,15 @@
       <div class="collapse" id="ui-menu-accounting">
         <ul class="nav flex-column sub-menu">
           <li class="nav-item"> 
-		<a class="nav-link" href="pages/ui-features/buttons.html">
-			Create Year
-		</a>
-	</li>
-	<li class="nav-item"> 
-		<a class="nav-link" href="pages/ui-features/buttons.html">
-			Create Month
-		</a>
-	</li>
+        		<a class="nav-link" href="{!! url('et/accounting/create-year') !!}">
+        			Create Year
+        		</a>
+        	</li>
+        	<li class="nav-item"> 
+        		<a class="nav-link" href="{!! url('et/accounting/create-month') !!}">
+        			Create Month
+        		</a>
+        	</li>
         </ul>
       </div>
     </li>
