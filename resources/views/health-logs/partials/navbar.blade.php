2a47614eb2017231eaba6422ef4a0f4050029edc
Kadir <razu121@gmail.com>
2018-12-04 15:43:03 +0600

    From Office Laptop, 04:12:18 @ 03:42PM

--- resources/views/health-logs/partials/navbar.blade.php
+++ resources/views/health-logs/partials/navbar.blade.php
@@ -2,7 +2,8 @@
 <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
   <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
     <a class="navbar-brand brand-logo" href="{!! url('et/dashboard') !!}">
-    	Health Logs 
+    	<img src="{!! asset('theme/images/favicon-hl.png') !!}" alt="Logo" class="text-logo">
+      Health Logs 
     </a>
     <a class="navbar-brand brand-logo-mini" href="index.html">
 	<img src="images/logo-mini.svg" alt="logo"/>
